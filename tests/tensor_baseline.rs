b166fdb5d7c362437f6d82eb20316a1e0b497fa1
Burner <burner@localhost>
2026-08-14 06:17:11 -0700

    burner: Expose the read-only Tensor.is_sparse_csr property

--- tests/tensor_baseline.rs
+++ tests/tensor_baseline.rs
@@ -39,6 +39,7 @@ fn native_metadata_describes_all_supported_storage_shapes() {
         assert!(!tensor.is_complex());
         assert!(!tensor.is_quantized());
         assert!(!tensor.is_sparse());
+        assert!(!tensor.is_sparse_csr());
         assert!(tensor.is_signed());
     }
 }
@@ -68,6 +69,7 @@ fn native_metadata_survives_views_kernels_and_reductions() {
         assert!(!output.is_complex());
         assert!(!output.is_quantized());
         assert!(!output.is_sparse());
+        assert!(!output.is_sparse_csr());
         assert!(output.is_signed());
     }
 }
