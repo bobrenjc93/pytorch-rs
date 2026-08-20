ff2c4c4120905a15c1e8cfb18a0074c330f6caba
Burner <burner@localhost>
2026-08-20 03:18:47 -0700

    burner: address review round 1

--- tests/test_serialization_get_default_mmap_options.py
+++ tests/test_serialization_get_default_mmap_options.py
@@ -112,7 +112,9 @@ class SerializationGetDefaultMmapOptionsTests(unittest.TestCase):
         self.assertEqual(function.__qualname__, "get_default_mmap_options")
         self.assertEqual(function.__module__, "torch_rs.serialization")
         self.assertIs(inspect.getmodule(function), serialization)
-        self.assertEqual(function.__doc__, FUNCTION_DOC)
+        self.assertEqual(
+            inspect.cleandoc(function.__doc__), inspect.cleandoc(FUNCTION_DOC)
+        )
         self.assertIsNone(function.__defaults__)
         self.assertIsNone(function.__kwdefaults__)
         self.assertEqual(function.__dict__, {})
