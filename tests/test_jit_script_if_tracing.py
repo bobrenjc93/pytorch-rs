c41046bc388b19e0ad3a2ea2d26dfcfd1a1455b0
Burner <burner@localhost>
2026-08-20 16:02:11 -0700

    burner: address review round 1

--- tests/test_jit_script_if_tracing.py
+++ tests/test_jit_script_if_tracing.py
@@ -210,7 +210,9 @@ class JitScriptIfTracingTests(unittest.TestCase):
         self.assertEqual(function.__qualname__, "script_if_tracing")
         self.assertEqual(function.__module__, "torch_rs.jit")
         self.assertIs(inspect.getmodule(function), jit)
-        self.assertEqual(function.__doc__, FUNCTION_DOC)
+        self.assertEqual(
+            inspect.cleandoc(function.__doc__), inspect.cleandoc(FUNCTION_DOC)
+        )
         self.assertIsNone(function.__defaults__)
         self.assertIsNone(function.__kwdefaults__)
         self.assertEqual(function.__dict__, {})
