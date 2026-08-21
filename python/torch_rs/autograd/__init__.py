cf894e60532f4daaf4822061a66b86baacd1ae30
Burner <burner@localhost>
2026-08-21 13:19:03 -0700

    burner: Expose autograd multithreading default-state query

--- python/torch_rs/autograd/__init__.py
+++ python/torch_rs/autograd/__init__.py
@@ -5,6 +5,7 @@ from . import grad_mode as grad_mode
 from .grad_mode import no_grad as no_grad
 
 
+is_multithreading_enabled = _C._is_multithreading_enabled
 is_view_replay_enabled = _C._is_view_replay_enabled
 
 __all__ = ["grad_mode", "no_grad"]
