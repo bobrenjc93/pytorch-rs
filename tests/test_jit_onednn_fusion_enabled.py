57998fbc4d30d0f36817286af457ff655839369c
Burner <burner@localhost>
2026-08-21 21:46:31 -0700

    burner: address review round 1

--- tests/test_jit_onednn_fusion_enabled.py
+++ tests/test_jit_onednn_fusion_enabled.py
@@ -98,7 +98,9 @@ class JitOnednnFusionEnabledTests(unittest.TestCase):
         self.assertEqual(function.__qualname__, "onednn_fusion_enabled")
         self.assertEqual(function.__module__, "torch_rs.jit")
         self.assertIs(inspect.getmodule(function), jit)
-        self.assertEqual(function.__doc__, FUNCTION_DOC)
+        self.assertEqual(
+            inspect.cleandoc(function.__doc__), inspect.cleandoc(FUNCTION_DOC)
+        )
         self.assertIsNone(function.__defaults__)
         self.assertIsNone(function.__kwdefaults__)
         self.assertEqual(function.__dict__, {})
