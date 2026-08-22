0c7718aa5efbaf30b2ca5811c562f6f11b977bf8
Burner <burner@localhost>
2026-08-21 23:19:42 -0700

    burner: Add non-NumPy torch.utils.data.default_convert

--- tests/test_sampler.py
+++ tests/test_sampler.py
@@ -116,6 +116,7 @@ class SamplerTests(unittest.TestCase):
                 "StackDataset",
                 "Subset",
                 "TensorDataset",
+                "default_convert",
                 "get_worker_info",
             ],
         )
