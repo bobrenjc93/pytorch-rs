8a79928d0e94483275631204bc31d25d6f8d4e3d
Burner <burner@localhost>
2026-08-23 11:50:26 -0700

    burner: Add leaf-only torch.utils.data.default_convert

--- tests/test_batch_sampler_reference.py
+++ tests/test_batch_sampler_reference.py
@@ -213,6 +213,7 @@ class BatchSamplerReferenceTests(unittest.TestCase):
                     "StackDataset",
                     "Subset",
                     "TensorDataset",
+                    "default_convert",
                     "get_worker_info",
                 }
             ],
