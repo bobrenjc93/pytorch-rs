b58d8d019b5f0669aa8d04f81eac360be9fc54b4
Burner <burner@localhost>
2026-08-26 14:09:47 -0700

    burner: address review round 2

--- tests/test_compiler_skip_all_guards_unsafe.py
+++ tests/test_compiler_skip_all_guards_unsafe.py
@@ -233,7 +233,9 @@ class CompilerSkipAllGuardsUnsafeTests(unittest.TestCase):
         self.assertEqual(function.__qualname__, "skip_all_guards_unsafe")
         self.assertEqual(function.__module__, "torch_rs.compiler")
         self.assertIs(inspect.getmodule(function), compiler)
-        self.assertEqual(function.__doc__, FUNCTION_DOC)
+        self.assertEqual(
+            inspect.cleandoc(function.__doc__), inspect.cleandoc(FUNCTION_DOC)
+        )
         self.assertIsNone(function.__defaults__)
         self.assertIsNone(function.__kwdefaults__)
         self.assertEqual(function.__dict__, {})
