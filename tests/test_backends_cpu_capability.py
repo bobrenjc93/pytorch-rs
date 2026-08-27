b958381a10e81b3a94ce8d4601654943acd332e1
Burner <burner@localhost>
2026-08-27 16:38:25 -0700

    burner: address review round 1

--- tests/test_backends_cpu_capability.py
+++ tests/test_backends_cpu_capability.py
@@ -120,7 +120,10 @@ class CpuCapabilityTests(unittest.TestCase):
         self.assertEqual(function.__qualname__, "get_cpu_capability")
         self.assertEqual(function.__module__, "torch_rs.backends.cpu")
         self.assertIs(inspect.getmodule(function), cpu)
-        self.assertEqual(function.__doc__, FUNCTION_DOC)
+        self.assertEqual(
+            inspect.cleandoc(function.__doc__),
+            inspect.cleandoc(FUNCTION_DOC),
+        )
         self.assertIsNone(function.__defaults__)
         self.assertIsNone(function.__kwdefaults__)
         self.assertEqual(function.__dict__, {})
